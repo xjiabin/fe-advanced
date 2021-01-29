10dcbd2a441fe5146a4fc6fd18f03409326661ef
xjiabin <18344164480@163.com>
2021-01-29 22:15:10 +0800

    部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ const question = require('./routers/question')
 module.exports = {
   title: '前端进阶',
   description: '记录前端知识点与面试题',
+  base: '/fe-advanced/',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
