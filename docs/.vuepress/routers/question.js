d50bafdd2b2e4df7a926cb7e8eba6722972e2ddc
jiabin <18344164480@163.com>
2021-02-01 13:53:14 +0800

    【添加侧边栏路由】

--- docs/.vuepress/routers/question.js
+++ docs/.vuepress/routers/question.js
@@ -8,17 +8,18 @@ module.exports = [
   // },
   {
     title: 'JS 基础',
-    children: [
-      '/question/js/01',
-      '/question/js/02',
-      '/question/js/03'
-    ]
+    children: ['/question/js/01', '/question/js/02', '/question/js/03']
   },
   {
     title: '编程题',
     children: [
       '/question/code/01',
-      '/question/code/02'
+      '/question/code/02',
+      '/question/code/03',
+      '/question/code/04',
+      '/question/code/05',
+      '/question/code/07',
+      '/question/code/06'
     ]
   }
 ]
